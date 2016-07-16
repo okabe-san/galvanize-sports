a7ea70f3c28b7bb51d97c28a38ea39bc24209d29
Akiko Okabe <aki0u0@gmail.com>
2016-07-16 11:34:10 -0600

    first commit

--- galvanize_sports.js
+++ galvanize_sports.js
@@ -5,26 +5,58 @@ var shoppingCart = data.shoppingCart;
 module.exports = {
     inventory: data.inventory,
     shoppingCart: data.shoppingCart,
-    addItem: function(itemId, quantity){
-        // Your code here!
+    addItem: function(itemId, quant){
+
+       for (var i = 0; i < inventory.length; i++) {
+         if (inventory[i].id === parseInt(itemId)) {
+           if (inventory[i].quantityAvailable < quant) {
+             shoppingCart[i].quantity += inventory[i].quantityAvailable;
+             inventory[i].quantityAvailable = 0;
+           } else {
+             inventory[i].quantityAvailable -= quant;
+             shoppingCart[i].quantity += quant;
+           }
+         }
+       }
     },
-    removeItem: function(itemId, quantity){
-        // Your code here!
+
+    removeItem: function(itemId, quant){
+
+      for (var i = 0; i < inventory.length; i++) {
+        if (shoppingCart[i].itemId === itemId) {
+          if (shoppingCart[i].quantity >= quant) {
+            inventory[i].quantityAvailable += quant;
+            shoppingCart[i].quantity -= quant;
+          } else {
+            inventory[i].quantityAvailable += shoppingCart[i].quantity;
+            shoppingCart[i].quantity = 0;
+          }
+        }
+      }
     },
+
     getCheckoutSubtotal: function(){
         var checkoutSubtotal = 0.00;
-        // Your code here!
+        for (var i = 0; i < inventory.length; i++) {
+          if (inventory[i].id === shoppingCart[i].itemId) {
+            checkoutSubtotal +=  shoppingCart[i].quantity * inventory[i].price;
+          }
+        }
         return checkoutSubtotal;
     },
+
     getTax: function(subtotal, rate){
         var tax = 0.00;
-        // Your code here!
+        tax += subtotal * rate;
         return tax;
     },
+
     getCheckoutTotal: function(){
         var TAX_RATE = 0.078;
         var checkoutTotal = 0.00;
-        // Your code here!
+
+        checkoutTotal = (this.getCheckoutSubtotal() + this.getTax(this.getCheckoutSubtotal(), TAX_RATE)).toFixed(2);
+
         return checkoutTotal;
     }
 }
